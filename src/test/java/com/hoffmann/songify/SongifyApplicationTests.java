dd062663ef8b8ddcf43182954039c6471481570a
Wiktoria Hoffmann <whoffmann2000@wp.pl>
2025-07-06 11:27:44 +0200

    first commit

---
+++ src/test/java/com/hoffmann/songify/SongifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoffmann.songify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
