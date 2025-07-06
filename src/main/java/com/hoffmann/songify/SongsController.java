760c83ede25fcd24b3682147bc98e7d4800da747
Wiktoria Hoffmann <whoffmann2000@wp.pl>
2025-07-06 19:46:07 +0200

    added thymeleaf basic frontend

--- src/main/java/com/hoffmann/songify/SongsController.java
+++ src/main/java/com/hoffmann/songify/SongsController.java
@@ -1,10 +1,9 @@
 package com.hoffmann.songify;
 
 import lombok.extern.log4j.Log4j2;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,22 +13,22 @@ import java.util.stream.Collectors;
 @Log4j2
 public class SongsController {
 
-    Map<Integer, String> database = new HashMap<>();
-    Logger logger = LoggerFactory.getLogger(SongsController.class);
+    Map<Integer, String> database = new HashMap<>(Map.of(
+            1, "Shawn Mendes song",
+            2, "Araia song",
+            3, "Shivers",
+            4, "Bad blood"
+    ));
 
     @GetMapping("/songs")
-    public ResponseEntity<SongResponseDto> getAllSongs(@RequestParam (required = false) Integer limit) {
-        database.put(1, "Shawn Mendes song");
-        database.put(2, "Araia song");
-        database.put(3, "Shivers");
-        database.put(4, "Bad blood");
+    public ResponseEntity<SongResponseDto> getAllSongs(@RequestParam(required = false) Integer limit) {
 
-        if(limit != null) {
+        if (limit != null) {
             Map<Integer, String> limitedMap = database.entrySet()
                     .stream()
                     .limit(limit)
                     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-            SongResponseDto response  = new SongResponseDto(limitedMap);
+            SongResponseDto response = new SongResponseDto(limitedMap);
             return ResponseEntity.ok(response);
         }
 
@@ -41,7 +40,7 @@ public class SongsController {
     public ResponseEntity<SingleSongReponseDto> getSongById(@PathVariable("id") Integer id, @RequestHeader(required = false) String requestId) {
         log.info(requestId);
         String song = database.get(id);
-        if(song == null) {
+        if (song == null) {
             return ResponseEntity.notFound().build();
         }
         SingleSongReponseDto response = new SingleSongReponseDto(song);
---
+++ src/main/java/com/hoffmann/songify/SongsViewController.java
@@ -0,0 +1,33 @@
+package com.hoffmann.songify;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class SongsViewController {
+
+    private Map<Integer, String> database = new HashMap<>(Map.of(
+            0, "Shawa Song",
+            1, "Arian Song",
+            2, "Nicky Song",
+            3, "Sabri Song",
+            4, "Shakira Song"
+    ));
+
+    @GetMapping("/")
+    public String home() {
+        return "home.html";
+    }
+
+    @GetMapping("/view/songs")
+    public String songs(Model model) {
+
+
+        model.addAttribute("database", database);
+        return "songs.html";
+    }
+}
